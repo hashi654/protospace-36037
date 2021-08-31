e5d70b44466b57f3b8292a33409e1ec77f46f474
hashi654 <clsy.engineering@gmail.com>
2021-08-31 23:11:09 +0900

    アクションとルーティングの設定、ビューファイルの設置

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'prototypes/index'
   root to: "prototypes#index"
+  resources :prototypes, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
